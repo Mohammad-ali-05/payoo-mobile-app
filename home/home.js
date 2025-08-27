40e27425ef7e79e8e97bba8945f01121b778cd46
Mohammad Ali <muhammadali.pass1@gmail.com>
2025-08-27 15:35:25 +0600

    Completed pay bill section

--- home/home.js
+++ home/home.js
@@ -32,13 +32,15 @@ function resetInputValue(ids) {
 }
 //Hiding all the section
 function allSectionHide() {
-    const navigation = document.getElementsByClassName("navigation");
-    for (const nav of navigation) {
-        nav.style.display = "none"
-    }
+  const navigation = document.getElementsByClassName("navigation");
+  for (const nav of navigation) {
+    nav.style.display = "none";
+  }
 }
 // Add money section functionality
-document.getElementById("add-money-btn").addEventListener("click", function (e) {
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
     e.preventDefault();
     if (mobileNumber !== mobileNumberConverted("add-money-number")) {
       alert("Account number is invalid");
@@ -59,31 +61,31 @@ document.getElementById("add-money-btn").addEventListener("click", function (e) 
 
 //Cash out section functionality
 document.getElementById("cash-out-btn").addEventListener("click", function (e) {
-    e.preventDefault()
-    if (mobileNumber !== mobileNumberConverted("cash-out-number")) {
-      alert("Mobile number is invalid");
-    } else if (pinNumber !== pinNumberConverted("cash-out-pin")) {
-      alert("Pin number is invalid");
-    } else if (
-      isNaN(amountConverted("cash-out-amount")) ||
-      amountConverted("cash-out-amount") < 0
-    ) {
-      alert("Invalid amount given");
+  e.preventDefault();
+  if (mobileNumber !== mobileNumberConverted("cash-out-number")) {
+    alert("Mobile number is invalid");
+  } else if (pinNumber !== pinNumberConverted("cash-out-pin")) {
+    alert("Pin number is invalid");
+  } else if (
+    isNaN(amountConverted("cash-out-amount")) ||
+    amountConverted("cash-out-amount") < 0
+  ) {
+    alert("Invalid amount given");
+  } else {
+    if (balanceConverted() < amountConverted("cash-out-amount")) {
+      alert("Insufficient balance");
     } else {
-        if (balanceConverted() < amountConverted("cash-out-amount")) {
-            alert("Insufficient balance")
-        } else {
-            const newBalance =
-              balanceConverted() - amountConverted("cash-out-amount");
-            document.getElementById("balance").innerText = newBalance;
-        }
+      const newBalance =
+        balanceConverted() - amountConverted("cash-out-amount");
+      document.getElementById("balance").innerText = newBalance;
     }
-    resetInputValue(["cash-out-number", "cash-out-amount", "cash-out-pin"]);
+  }
+  resetInputValue(["cash-out-number", "cash-out-amount", "cash-out-pin"]);
 });
 
 //Transfer money section functionality
 document.getElementById("transfer-money-btn").addEventListener("click", function (e) {
-    e.preventDefault()
+    e.preventDefault();
     if (mobileNumber !== mobileNumberConverted("transfer-money-number")) {
       alert("Mobile number is invalid");
     } else if (pinNumber !== pinNumberConverted("transfer-money-pin")) {
@@ -107,25 +109,72 @@ document.getElementById("transfer-money-btn").addEventListener("click", function
       "transfer-money-amount",
       "transfer-money-pin",
     ]);
+  });
+
+//Pay bill section functionality
+document.getElementById("pay-bill-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("i am connected")
+  if (mobileNumber !== mobileNumberConverted("pay-bill-number")) {
+    alert("Mobile number is invalid");
+  } else if (pinNumber !== pinNumberConverted("pay-bill-pin")) {
+    alert("Pin number is invalid");
+  } else if (
+    isNaN(amountConverted("pay-bill-amount")) ||
+    amountConverted("pay-bill-amount") < 0
+  ) {
+    alert("Invalid amount given");
+  } else {
+    if (balanceConverted() < amountConverted("pay-bill-amount")) {
+      alert("Insufficient balance");
+    } else {
+      const newBalance =
+        balanceConverted() - amountConverted("pay-bill-amount");
+      document.getElementById("balance").innerText = newBalance;
+    }
+  }
+  resetInputValue(["pay-bill-number", "pay-bill-amount", "pay-bill-pin"]);
 });
 
 //Add money button functionality
-document.getElementById("add-money-nav-btn").addEventListener("click", function (e) {
-    e.preventDefault()
-    allSectionHide()
+document
+  .getElementById("add-money-nav-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    allSectionHide();
     document.getElementById("add-money-section").style.display = "block";
-});
+  });
 
 // Cash out button functionality
-document.getElementById("cash-out-nav-btn").addEventListener("click", function (e) {
-    e.preventDefault()
+document
+  .getElementById("cash-out-nav-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
     allSectionHide();
     document.getElementById("cash-out-section").style.display = "block";
-});
+  });
 
 //Transfer money button functionality
-document.getElementById("transfer-money-nav-btn").addEventListener("click", function (e) {
-    e.preventDefault()
-    allSectionHide()
+document
+  .getElementById("transfer-money-nav-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    allSectionHide();
     document.getElementById("transfer-money-section").style.display = "block";
+  });
+
+// Get bonus button functionality
+document
+  .getElementById("get-bonus-nav-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    allSectionHide();
+    document.getElementById("get-bonus-section").style.display = "block";
+  });
+
+//Pay bill button functionality
+document.getElementById("pay-bill-nav-btn").addEventListener("click", function (e) {
+  e.preventDefault()
+  allSectionHide();
+  document.getElementById("pay-bill-section").style.display = "block";
 });
